f62eff9467a494865a431c9134950f59619d55e0
wluyima <wyclif@openmrs.org>
2022-08-31 13:53:52 -0500

    TFS-338445 - Added more web code

---
+++ omod/src/main/java/org/openmrs/module/patientgrid/web/rest/v1_0/controller/PatientGridSubResourceController.java
@@ -0,0 +1,20 @@
+package org.openmrs.module.patientgrid.web.rest.v1_0.controller;
+
+import org.openmrs.module.patientgrid.web.rest.v1_0.PatientGridRestConstants;
+import org.openmrs.module.webservices.rest.web.v1_0.controller.MainSubResourceController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/rest/" + PatientGridRestConstants.NAMESPACE)
+public class PatientGridSubResourceController extends MainSubResourceController {
+	
+	/**
+	 * @see MainSubResourceController#getNamespace()
+	 */
+	@Override
+	public String getNamespace() {
+		return PatientGridRestConstants.NAMESPACE;
+	}
+	
+}
