debf1e7b78e1ca7c229893b7370fce670859ceb2
wluyima <wyclif@openmrs.org>
2022-09-08 12:45:00 -0500

    TFS-336803 - Re-implemented Latest Obs Cohort definitin to fetch matching obs only from the most recent encounter

--- api/src/main/java/org/openmrs/module/patientgrid/filter/LatestObsCohortDefinitionEvaluator.java
+++ api/src/main/java/org/openmrs/module/patientgrid/filter/LatestObsCohortDefinitionEvaluator.java
@@ -1,16 +1,14 @@
 package org.openmrs.module.patientgrid.filter;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.hibernate.Criteria;
+import org.hibernate.Query;
 import org.hibernate.SessionFactory;
-import org.hibernate.criterion.ProjectionList;
 import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
 import org.openmrs.Cohort;
 import org.openmrs.Obs;
-import org.openmrs.Person;
 import org.openmrs.annotation.Handler;
 import org.openmrs.module.reporting.cohort.EvaluatedCohort;
 import org.openmrs.module.reporting.cohort.definition.CohortDefinition;
@@ -22,37 +20,41 @@ import org.springframework.beans.factory.annotation.Autowired;
 @Handler(supports = LatestObsCohortDefinition.class, order = 50)
 public class LatestObsCohortDefinitionEvaluator implements CohortDefinitionEvaluator {
 	
-	@Autowired
+	//Unfortunately hibernate's criteria API and HQL don't support joins to a derived table with multiple columns
+	private final static String MOST_RECENT_DATES = "SELECT e.patient_id, max(e.encounter_datetime) AS maxDate FROM encounter e, "
+	        + "patient p WHERE e.patient_id = p.patient_id AND e.encounter_type = :encType AND e.voided = false AND"
+	        + " p.voided = false GROUP BY e.patient_id";
+	
+	private final static String MOST_RECENT_ENC_IDS = "SELECT e1.encounter_id FROM encounter e1 INNER JOIN ("
+	        + MOST_RECENT_DATES + ") e2 ON e1.patient_id = e2.patient_id AND e1.encounter_datetime = e2.maxDate";
+	
 	private SessionFactory sf;
+	
+	@Autowired
+	public LatestObsCohortDefinitionEvaluator(SessionFactory sf) {
+		this.sf = sf;
+	}
 	
 	@Override
 	public EvaluatedCohort evaluate(CohortDefinition cohortDefinition, EvaluationContext evaluationContext)
 	    throws EvaluationException {
 		
-		//TODO use a sub query to fetch only personIds and move voided restriction
 		LatestObsCohortDefinition cohortDef = (LatestObsCohortDefinition) cohortDefinition;
+		Query query = sf.getCurrentSession().createSQLQuery(MOST_RECENT_ENC_IDS);
+		query.setParameter("encType", cohortDef.getEncounterType());
+		List<Integer> encounterIds = query.list();
+		
 		Criteria criteria = sf.getCurrentSession().createCriteria(Obs.class, "o");
+		criteria.createCriteria("person", "p");
+		criteria.setProjection(Projections.property("p.personId"));
 		criteria.add(Restrictions.eq("o.concept", cohortDef.getConcept()));
 		criteria.add(Restrictions.eq("o.voided", false));
 		criteria.add(Restrictions.in("o." + cohortDef.getPropertyName(), cohortDef.getValues()));
-		ProjectionList pl = Projections.projectionList();
-		pl.add(Projections.groupProperty("person"));
-		pl.add(Projections.max("obsDatetime"), "maxObsDatetime");
-		criteria.setProjection(pl);
-		
-		criteria.createCriteria("person", "p");
-		criteria.add(Restrictions.eq("p.voided", false));
 		
 		criteria.createCriteria("encounter", "e");
-		criteria.add(Restrictions.isNotNull("o.encounter"));
-		criteria.add(Restrictions.eq("e.encounterType", cohortDef.getEncounterType()));
-		criteria.add(Restrictions.eq("e.voided", false));
+		criteria.add(Restrictions.in("e.encounterId", encounterIds));
 		
-		List<Object[]> resultRows = criteria.list();
-		List<Integer> patientIds = new ArrayList(resultRows.size());
-		resultRows.stream().forEach(row -> patientIds.add(((Person) row[0]).getId()));
-		
-		return new EvaluatedCohort(new Cohort(patientIds), cohortDefinition, evaluationContext);
+		return new EvaluatedCohort(new Cohort(criteria.list()), cohortDefinition, evaluationContext);
 	}
 	
 }
