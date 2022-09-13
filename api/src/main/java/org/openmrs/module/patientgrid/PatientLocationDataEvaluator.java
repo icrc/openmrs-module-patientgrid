7d1dd94b58e3d2688e2a25de8e33265678c18182
wluyima <wyclif@openmrs.org>
2022-09-13 11:22:49 -0500

    Removed not needed PatientLocationEvaluatorUtils class

--- api/src/main/java/org/openmrs/module/patientgrid/PatientLocationDataEvaluator.java
+++ api/src/main/java/org/openmrs/module/patientgrid/PatientLocationDataEvaluator.java
@@ -1,11 +1,23 @@
 package org.openmrs.module.patientgrid;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.openmrs.Location;
+import org.openmrs.Patient;
 import org.openmrs.annotation.Handler;
+import org.openmrs.api.LocationService;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.datafilter.impl.EntityBasisMap;
 import org.openmrs.module.reporting.data.patient.EvaluatedPatientData;
 import org.openmrs.module.reporting.data.patient.definition.PatientDataDefinition;
 import org.openmrs.module.reporting.data.patient.evaluator.PatientDataEvaluator;
 import org.openmrs.module.reporting.evaluation.EvaluationContext;
 import org.openmrs.module.reporting.evaluation.EvaluationException;
+import org.openmrs.module.reporting.evaluation.querybuilder.HqlQueryBuilder;
+import org.openmrs.module.reporting.evaluation.service.EvaluationService;
 
 @Handler(supports = PatientLocationDataDefinition.class, order = 50)
 public class PatientLocationDataEvaluator implements PatientDataEvaluator {
@@ -14,8 +26,33 @@ public class PatientLocationDataEvaluator implements PatientDataEvaluator {
 	public EvaluatedPatientData evaluate(PatientDataDefinition definition, EvaluationContext context)
 	        throws EvaluationException {
 		
+		HqlQueryBuilder q = new HqlQueryBuilder();
+		q.select("e.entityIdentifier", "e.basisIdentifier", "e.dateCreated");
+		q.from(EntityBasisMap.class, "e");
+		q.whereEqual("e.basisType", Location.class.getName());
+		q.whereEqual("e.entityType", Patient.class.getName());
+		q.wherePatientIn("e.entityIdentifier", context);
+		
+		Map<Integer, Date> patientIdAndMapDateCreated = new HashMap();
+		List<Object[]> rows = Context.getService(EvaluationService.class).evaluateToList(q, context);
+		LocationService locationService = Context.getLocationService();
+		Map<Integer, Object> patientIdAndLocationMap = new HashMap(rows.size());
+		for (Object[] row : rows) {
+			Integer patientId = Integer.valueOf(row[0].toString());
+			Date mapDateCreated = (Date) row[2];
+			//In case of multiple mapped locations, pick most recently added
+			if (patientIdAndLocationMap.containsKey(patientId)
+			        && patientIdAndMapDateCreated.get(patientId).after(mapDateCreated)) {
+				
+				continue;
+			}
+			
+			patientIdAndLocationMap.put(patientId, locationService.getLocation(Integer.valueOf(row[1].toString())));
+			patientIdAndMapDateCreated.put(patientId, mapDateCreated);
+		}
+		
 		EvaluatedPatientData data = new EvaluatedPatientData(definition, context);
-		data.setData(PatientLocationEvaluatorUtils.evaluate(definition, context));
+		data.setData(patientIdAndLocationMap);
 		
 		return data;
 	}
--- api/src/test/java/org/openmrs/module/patientgrid/PatientLocationDataEvaluatorTest.java
+++ api/src/test/java/org/openmrs/module/patientgrid/PatientLocationDataEvaluatorTest.java
@@ -2,10 +2,19 @@ package org.openmrs.module.patientgrid;
 
 import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
 
 import org.junit.Test;
 import org.openmrs.Cohort;
+import org.openmrs.Location;
+import org.openmrs.Patient;
 import org.openmrs.api.LocationService;
+import org.openmrs.api.PatientService;
+import org.openmrs.module.datafilter.impl.EntityBasisMap;
+import org.openmrs.module.datafilter.impl.api.DataFilterService;
 import org.openmrs.module.reporting.data.patient.EvaluatedPatientData;
 import org.openmrs.module.reporting.data.patient.service.PatientDataService;
 import org.openmrs.module.reporting.evaluation.EvaluationContext;
@@ -20,6 +29,12 @@ public class PatientLocationDataEvaluatorTest extends BaseModuleContextSensitive
 	@Autowired
 	private LocationService locationService;
 	
+	@Autowired
+	private PatientService patientService;
+	
+	@Autowired
+	private DataFilterService dataFilterService;
+	
 	@Test
 	public void evaluate_shouldReturnThePatientLocation() throws Exception {
 		executeDataSet("entityBasisMaps.xml");
@@ -33,6 +48,54 @@ public class PatientLocationDataEvaluatorTest extends BaseModuleContextSensitive
 		assertEquals(2, data.getData().size());
 		assertEquals(locationService.getLocation(4000), data.getData().get(patientId2));
 		assertEquals(locationService.getLocation(4001), data.getData().get(patientId6));
+	}
+	
+	@Test
+	public void evaluate_shouldReturnNullForAPatientWithNoMappedLocation() throws Exception {
+		executeDataSet("entityBasisMaps.xml");
+		final Integer patientId = 999;
+		Patient patient = patientService.getPatient(patientId);
+		assertNotNull(patient);
+		assertTrue(dataFilterService.getEntityBasisMaps(patient, Location.class.getName()).isEmpty());
+		EvaluationContext context = new EvaluationContext();
+		context.setBaseCohort(new Cohort(asList(patientId)));
+		assertTrue(patientDataService.evaluate(new PatientLocationDataDefinition(), context).getData().isEmpty());
+	}
+	
+	@Test
+	public void evaluate_shouldReturnTheLocationAssociatedToTheMostRecentMappingInCaseOfMoreThanOne() throws Exception {
+		executeDataSet("entityBasisMaps.xml");
+		final Integer patientId = 7;
+		Patient patient = patientService.getPatient(patientId);
+		List<EntityBasisMap> maps = (List) dataFilterService.getEntityBasisMaps(patient, Location.class.getName());
+		assertEquals(3, maps.size());
+		assertTrue(maps.get(0).getDateCreated().before(maps.get(1).getDateCreated()));
+		EvaluationContext context = new EvaluationContext();
+		context.setBaseCohort(new Cohort(asList(patientId)));
+		PatientLocationDataDefinition d = new PatientLocationDataDefinition();
+		
+		EvaluatedPatientData data = patientDataService.evaluate(d, context);
+		
+		assertEquals(1, data.getData().size());
+		assertEquals(locationService.getLocation(4001), data.getData().get(patientId));
+	}
+	
+	@Test
+	public void evaluate_shouldReturnTheLocationAssociatedToTheLastAddedMappingInCaseOfMoreThanOneWithSameDateCreated()
+	        throws Exception {
+		executeDataSet("entityBasisMaps.xml");
+		final Integer patientId = 432;
+		Patient patient = patientService.getPatient(patientId);
+		List<EntityBasisMap> maps = (List) dataFilterService.getEntityBasisMaps(patient, Location.class.getName());
+		assertEquals(2, maps.size());
+		assertEquals(maps.get(0).getDateCreated(), (maps.get(1).getDateCreated()));
+		EvaluationContext context = new EvaluationContext();
+		context.setBaseCohort(new Cohort(asList(patientId)));
+		
+		EvaluatedPatientData data = patientDataService.evaluate(new PatientLocationDataDefinition(), context);
+		
+		assertEquals(1, data.getData().size());
+		assertEquals(locationService.getLocation(4002), data.getData().get(patientId));
 	}
 	
 }
