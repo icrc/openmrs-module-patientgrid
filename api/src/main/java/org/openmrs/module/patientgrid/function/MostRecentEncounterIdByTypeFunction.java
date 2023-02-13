13eaa9c5a7ac99d4916345af2dfcf5ec8d3cd575
saurabh-hirugade <saurabh.hirugade@thoughtworks.com>
2023-02-13 11:57:04 +0530

    Refactor. Query in MostRecentEncounterIdByTypeFunction

--- api/src/main/java/org/openmrs/module/patientgrid/function/MostRecentEncounterIdByTypeFunction.java
+++ api/src/main/java/org/openmrs/module/patientgrid/function/MostRecentEncounterIdByTypeFunction.java
@@ -11,43 +11,44 @@ import java.util.List;
 import java.util.function.Function;
 
 public class MostRecentEncounterIdByTypeFunction implements Function<EncounterType, List> {
-	
-	private final static String MOST_RECENT_DATES = "SELECT e.patient_id, max(e.encounter_datetime) AS maxDate FROM encounter e, "
-	        + "patient p WHERE e.patient_id = p.patient_id AND e.encounter_type = :encType AND e.voided = false AND"
-	        + " p.voided = false GROUP BY e.patient_id";
-	
-	private final static String MOST_RECENT_DATES_WITHIN_PERIODRANGE = "SELECT e.patient_id, max(e.encounter_datetime) AS maxDate FROM encounter e, "
-	        + "patient p WHERE e.patient_id = p.patient_id AND e.encounter_type = :encType AND e.voided = false AND"
-	        + " p.voided = false AND (e.encounter_datetime BETWEEN :fromDate AND :toDate) GROUP BY e.patient_id";
-	
-	private final String MOST_RECENT_ENC_IDS = this.periodRange == null
-	        ? "SELECT e1.encounter_id FROM encounter e1 INNER JOIN (" + MOST_RECENT_DATES
-	                + ") e2 ON e1.patient_id = e2.patient_id AND e1.encounter_datetime = e2.maxDate"
-	        : "SELECT e1.encounter_id FROM encounter e1 INNER JOIN (" + MOST_RECENT_DATES_WITHIN_PERIODRANGE
-	                + ") e2 ON e1.patient_id = e2.patient_id AND e1.encounter_datetime = e2.maxDate";
-	
-	private PeriodRange periodRange;
-	
-	private final PeriodRange periodRange;
-	
-	private SessionFactory sf;
-	
-	private static final Logger log = LoggerFactory.getLogger(MostRecentEncounterIdByTypeFunction.class);
-	
-	public MostRecentEncounterIdByTypeFunction(SessionFactory sf, PeriodRange periodRange) {
-		this.sf = sf;
-		this.periodRange = periodRange;
-	}
-	
-	@Override
-	public List apply(EncounterType encounterType) {
-		log.debug("Loading ids for the latest patient encounters of type: {}", encounterType);
-		Query query = sf.getCurrentSession().createSQLQuery(MOST_RECENT_ENC_IDS);
-		query.setParameter("encType", encounterType);
-		if (periodRange != null) {
-			query.setParameter("fromDate", periodRange.getFromDate());
-			query.setParameter("toDate", periodRange.getToDate());
-		}
-		return query.list();
-	}
+
+    private PeriodRange periodRange;
+
+    private SessionFactory sf;
+
+    private static final Logger log = LoggerFactory.getLogger(MostRecentEncounterIdByTypeFunction.class);
+
+    public MostRecentEncounterIdByTypeFunction(SessionFactory sf, PeriodRange periodRange) {
+        this.sf = sf;
+        this.periodRange = periodRange;
+    }
+
+    @Override
+    public List apply(EncounterType encounterType) {
+        log.debug("Loading ids for the latest patient encounters of type: {}", encounterType);
+        Query query = sf.getCurrentSession().createSQLQuery(getQuery(periodRange));
+        query.setParameter("encType", encounterType);
+        if (periodRange != null) {
+            query.setParameter("fromDate", periodRange.getFromDate());
+            query.setParameter("toDate", periodRange.getToDate());
+        }
+        return query.list();
+    }
+
+    private String getQuery(PeriodRange periodRange) {
+        final String MOST_RECENT_DATES = "SELECT e.patient_id, max(e.encounter_datetime) AS maxDate FROM encounter e, "
+                + "patient p WHERE e.patient_id = p.patient_id AND e.encounter_type = :encType AND e.voided = false AND"
+                + " p.voided = false GROUP BY e.patient_id";
+
+        final String MOST_RECENT_DATES_WITHIN_PERIODRANGE = "SELECT e.patient_id, max(e.encounter_datetime) AS maxDate FROM encounter e, "
+                + "patient p WHERE e.patient_id = p.patient_id AND e.encounter_type = :encType AND e.voided = false AND"
+                + " p.voided = false AND (e.encounter_datetime BETWEEN :fromDate AND :toDate) GROUP BY e.patient_id";
+
+        if (periodRange != null) {
+            return "SELECT e1.encounter_id FROM encounter e1 INNER JOIN (" + MOST_RECENT_DATES_WITHIN_PERIODRANGE
+                    + ") e2 ON e1.patient_id = e2.patient_id AND e1.encounter_datetime = e2.maxDate";
+        }
+        return "SELECT e1.encounter_id FROM encounter e1 INNER JOIN (" + MOST_RECENT_DATES
+                + ") e2 ON e1.patient_id = e2.patient_id AND e1.encounter_datetime = e2.maxDate";
+    }
 }
