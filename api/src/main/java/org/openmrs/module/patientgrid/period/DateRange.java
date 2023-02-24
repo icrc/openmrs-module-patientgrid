5a5ee670dd2a58362c8d535ebe30ace75b83157b
Fred Deniger <fdeniger@icrc.org>
2023-02-24 19:20:17 +0100

    add support for report Metadata Improve cache management with Periods

--- api/src/main/java/org/openmrs/module/patientgrid/period/DateRange.java
+++ api/src/main/java/org/openmrs/module/patientgrid/period/DateRange.java
@@ -7,33 +7,33 @@ import org.openmrs.module.patientgrid.PatientGridUtils;
 import java.util.Date;
 
 public class DateRange {
-
-  private final String operand;
-
-  private final Date fromInServerTz;
-
-  private final Date toInServerTz;
-
-  public DateRange(String operand, Date fromInServerTz, Date toInServerTz) {
-    this.fromInServerTz = fromInServerTz;
-    this.toInServerTz = toInServerTz;
-    this.operand = operand;
-  }
-
-  public String getOperand() {
-    return operand;
-  }
-
-  public String getDateRangeAsString() {
-    return fromInServerTz.getTime() + "-" + toInServerTz.getTime();
-  }
-
-  public Date getToInServerTz() {
-    return toInServerTz;
-  }
-
-  public Date getFromInServerTz() {
-    return fromInServerTz;
-  }
-
+	
+	private final String operand;
+	
+	private final Date fromInServerTz;
+	
+	private final Date toInServerTz;
+	
+	public DateRange(String operand, Date fromInServerTz, Date toInServerTz) {
+		this.fromInServerTz = fromInServerTz;
+		this.toInServerTz = toInServerTz;
+		this.operand = operand;
+	}
+	
+	public String getOperand() {
+		return operand;
+	}
+	
+	public String getDateRangeAsString() {
+		return fromInServerTz.getTime() + "-" + toInServerTz.getTime();
+	}
+	
+	public Date getToInServerTz() {
+		return toInServerTz;
+	}
+	
+	public Date getFromInServerTz() {
+		return fromInServerTz;
+	}
+	
 }
